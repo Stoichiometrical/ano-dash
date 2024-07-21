427f3dc95a0c0b96f0668b9c7fdfab212ece7922
David Tendai Gondo <73646279+Stoichiometrical@users.noreply.github.com>
2024-07-21 11:37:34 +0400

    update

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import UsersTable from "./components/table/Table.jsx";
+
+
+function App() {
+
+
+  return (
+    <div className="">
+        <div className="text-center font-bold text-2xl">Mhanya</div>
+        <UsersTable/>
+
+
+
+    </div>
+  )
+}
+
+export default App
